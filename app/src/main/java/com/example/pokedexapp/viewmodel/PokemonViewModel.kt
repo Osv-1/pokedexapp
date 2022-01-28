a5b03a8d765a852c9448316f948e3e91aa9d52be
Takeo <oswaldotakeo1@gmail.com>
2022-01-28 09:21:02 -0300

    feat : add ViewModel

--- app/src/main/java/com/example/pokedexapp/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokedexapp/viewmodel/PokemonViewModel.kt
@@ -6,36 +6,26 @@ import com.example.pokedexapp.api.PokemonRepository
 import com.example.pokedexapp.domain.Pokemon
 
 class PokemonViewModel : ViewModel() {
-
     var pokemons = MutableLiveData<List<Pokemon?>>()
 
     init {
         Thread(Runnable {
             loadPokemons()
         }).start()
-
-        loadPokemons()
-
     }
 
     private fun loadPokemons() {
-        //fazendo a requisição
         val pokemonsApiResult = PokemonRepository.listPokemons()
 
-        //carregando os resultados
         pokemonsApiResult?.results?.let {
             pokemons.postValue(it.map { pokemonResult ->
-
-                //pegando numero do pokemon
-                val number =
-                    pokemonResult.url.replace("https://pokeapi.co/api/v2/pokemon", "")
-                        .replace("/", "").toInt()
-
+                val number = pokemonResult.url
+                    .replace("https://pokeapi.co/api/v2/pokemon/", "")
+                    .replace("/", "").toInt()
 
                 val pokemonApiResult = PokemonRepository.getPokemon(number)
 
                 pokemonApiResult?.let {
-
                     Pokemon(
                         pokemonApiResult.id,
                         pokemonApiResult.name,
@@ -43,16 +33,8 @@ class PokemonViewModel : ViewModel() {
                             type.type
                         }
                     )
-
-
                 }
-
-
             })
-
-
         }
-
     }
-
 }
